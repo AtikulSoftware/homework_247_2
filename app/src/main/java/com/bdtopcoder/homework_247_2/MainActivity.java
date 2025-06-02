57e2d138d4218ffb39267dd4e8af69f026d4b599
AtikulSoftware <atikul.official2020@gmail.com>
2025-06-02 13:39:35 +0600

    Play Audio From Url

--- app/src/main/java/com/bdtopcoder/homework_247_2/MainActivity.java
+++ app/src/main/java/com/bdtopcoder/homework_247_2/MainActivity.java
@@ -24,139 +24,145 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<HashMap<String, String>> categoryList = new ArrayList<>();
-    HashMap<String, String> categoryMap;
+   ArrayList<HashMap<String, String>> categoryList = new ArrayList<>();
+   HashMap<String, String> categoryMap;
 
-    ArrayList<HashMap<String, String>> songList = new ArrayList<>();
-    HashMap<String, String> songMap;
+   ArrayList<HashMap<String, String>> songList = new ArrayList<>();
+   HashMap<String, String> songMap;
 
-    // variable
-    GridView gridView;
+   // variable
+   GridView gridView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      EdgeToEdge.enable(this);
+      setContentView(R.layout.activity_main);
+      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+         return insets;
+      });
 
-        // initialize
-        gridView = findViewById(R.id.gridView);
+      // initialize
+      gridView = findViewById(R.id.gridView);
 
-        // load List
-        loadCategory();
+      // load List
+      loadCategory();
 
-        CategoryAdapter categoryAdapter = new CategoryAdapter();
-        gridView.setAdapter(categoryAdapter);
+      CategoryAdapter categoryAdapter = new CategoryAdapter();
+      gridView.setAdapter(categoryAdapter);
 
-    } // onCreate method end here ===========
-
-
-    public class CategoryAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return categoryList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            @SuppressLint("ViewHolder") View myView = layoutInflater.inflate(R.layout.grid_item, parent, false);
-
-            ImageView tvImage = myView.findViewById(R.id.tvImage);
-            TextView tvTitle = myView.findViewById(R.id.tvTitle);
-            CardView categoryItem = myView.findViewById(R.id.categoryItem);
-
-            HashMap<String, String> hashMap = categoryList.get(position);
-            String title = hashMap.get("title");
-            String catIcon = hashMap.get("catIcon");
-            String album = hashMap.get("album");
-
-            tvTitle.setText(title);
-
-            // item click
-            categoryItem.setOnClickListener(v -> {
-                if (album.equals("ArtcelAlbum")) {
-                    // go to Artcell Album
-                    loadArtcellAlbum();
-                    SongList.arrayList = songList;
-                    startActivity(new Intent(MainActivity.this, SongList.class));
-                } else if (album.equals("ShironamhinAlbum")) {
-                    // go to Artcell Album
-                    loadShironamhinAlbum();
-                    SongList.arrayList = songList;
-                    startActivity(new Intent(MainActivity.this, SongList.class));
-                }
-            });
-
-            return myView;
-        }
-
-    } // CategoryAdapter end here ===========
-
-    void loadCategory() {
-        //=== 1 === //
-        categoryMap = new HashMap<>();
-        categoryMap.put("title", "Artcell");
-        categoryMap.put("catIcon", String.valueOf(R.drawable.ic_launcher_background));
-        categoryMap.put("album", "ArtcelAlbum");
-        categoryList.add(categoryMap);
-
-        //=== 2 === //
-        categoryMap = new HashMap<>();
-        categoryMap.put("title", "Shironamhin");
-        categoryMap.put("catIcon", String.valueOf(R.drawable.ic_launcher_background));
-        categoryMap.put("album", "ShironamhinAlbum");
-        categoryList.add(categoryMap);
-    } // loadCategory end here =========
+   } // onCreate method end here ===========
 
 
-    void loadArtcellAlbum() {
-        songList.clear();
+   public class CategoryAdapter extends BaseAdapter {
 
-        songMap = new HashMap<>();
-        songMap.put("title", "Dukkho Bilash দুঃখ বিলাশ");
-        songMap.put("description", "Artcell");
-        songMap.put("songImg", String.valueOf(R.drawable.dukkho_bilash));
-        songMap.put("song", String.valueOf(R.raw.dukkho_bilash));
-        songList.add(songMap);
+      @Override
+      public int getCount() {
+         return categoryList.size();
+      }
 
-        songMap = new HashMap<>();
-        songMap.put("title", "Dhushor Shomoy || ধূসর সময় ");
-        songMap.put("description", "Artcell");
-        songMap.put("songImg", String.valueOf(R.drawable.dhushor_somoy));
-        songMap.put("song", String.valueOf(R.raw.dhushor_somoy));
-        songList.add(songMap);
+      @Override
+      public Object getItem(int position) {
+         return null;
+      }
 
-    } // loadArtcellAlbum end here ============
+      @Override
+      public long getItemId(int position) {
+         return 0;
+      }
 
-    void loadShironamhinAlbum() {
-        songList.clear();
+      @Override
+      public View getView(int position, View convertView, ViewGroup parent) {
+         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         @SuppressLint("ViewHolder") View myView = layoutInflater.inflate(R.layout.grid_item, parent, false);
 
-        songMap = new HashMap<>();
-        songMap.put("title", "Ei Obelay");
-        songMap.put("description", "Shironamhin");
-        songMap.put("songImg", String.valueOf(R.drawable.dukkho_bilash));
-        songMap.put("song", String.valueOf(R.raw.dukkho_bilash));
-        songList.add(songMap);
+         ImageView tvImage = myView.findViewById(R.id.tvImage);
+         TextView tvTitle = myView.findViewById(R.id.tvTitle);
+         CardView categoryItem = myView.findViewById(R.id.categoryItem);
 
-        songMap = new HashMap<>();
-        songMap.put("title", "Abar Hashimukh");
-        songMap.put("description", "Shironamhin");
-        songMap.put("songImg", String.valueOf(R.drawable.dhushor_somoy));
-        songMap.put("song", String.valueOf(R.raw.dhushor_somoy));
-        songList.add(songMap);
+         HashMap<String, String> hashMap = categoryList.get(position);
+         String title = hashMap.get("title");
+         String catIcon = hashMap.get("catIcon");
+         String album = hashMap.get("album");
 
-    } // loadArtcellAlbum end here ============
+         tvTitle.setText(title);
+
+         // item click
+         categoryItem.setOnClickListener(v -> {
+            if (album.equals("ArtcelAlbum")) {
+               // go to Artcell Album
+               loadArtcellAlbum();
+               SongList.arrayList = songList;
+               startActivity(new Intent(MainActivity.this, SongList.class));
+            } else if (album.equals("ShironamhinAlbum")) {
+               // go to Artcell Album
+               loadShironamhinAlbum();
+               SongList.arrayList = songList;
+               startActivity(new Intent(MainActivity.this, SongList.class));
+            }
+         });
+
+         return myView;
+      }
+
+   } // CategoryAdapter end here ===========
+
+   void loadCategory() {
+      //=== 1 === //
+      categoryMap = new HashMap<>();
+      categoryMap.put("title", "Artcell");
+      categoryMap.put("catIcon", String.valueOf(R.drawable.ic_launcher_background));
+      categoryMap.put("album", "ArtcelAlbum");
+      categoryList.add(categoryMap);
+
+      //=== 2 === //
+      categoryMap = new HashMap<>();
+      categoryMap.put("title", "Shironamhin");
+      categoryMap.put("catIcon", String.valueOf(R.drawable.ic_launcher_background));
+      categoryMap.put("album", "ShironamhinAlbum");
+      categoryList.add(categoryMap);
+   } // loadCategory end here =========
+
+
+   void loadArtcellAlbum() {
+      songList.clear();
+
+      songMap = new HashMap<>();
+      songMap.put("title", "Sample Audio 1");
+      songMap.put("description", "Artcell");
+      songMap.put("songImg", String.valueOf(R.drawable.dukkho_bilash));
+      songMap.put("song", "https://cdn.pixabay.com/audio/2025/05/29/audio_b92a610839.mp3");
+      songList.add(songMap);
+
+      songMap = new HashMap<>();
+      songMap.put("title", "Sample Audio 2");
+      songMap.put("description", "Artcell");
+      songMap.put("songImg", String.valueOf(R.drawable.dhushor_somoy));
+      songMap.put("song", "https://cdn.pixabay.com/audio/2025/05/21/audio_fa20813ea6.mp3");
+      songList.add(songMap);
+
+   } // loadArtcellAlbum end here ============
+
+   void loadShironamhinAlbum() {
+      songList.clear();
+
+      songMap = new HashMap<>();
+      songMap.put("title", "Sample Audio 1");
+      songMap.put("description", "Shironamhin");
+      songMap.put("songImg", String.valueOf(R.drawable.dukkho_bilash));
+      songMap.put("song", "https://cdn.pixabay.com/audio/2024/11/29/audio_45bbd49c34.mp3");
+      songList.add(songMap);
+
+      songMap = new HashMap<>();
+      songMap.put("title", "Sample Audio 2");
+      songMap.put("description", "Shironamhin");
+      songMap.put("songImg", String.valueOf(R.drawable.dhushor_somoy));
+      songMap.put("song", "https://cdn.pixabay.com/audio/2025/05/26/audio_904a54595f.mp3");
+      songList.add(songMap);
+
+   } // loadArtcellAlbum end here ============
 
 
 } // public class end here ==================
--- app/src/main/java/com/bdtopcoder/homework_247_2/SongList.java
+++ app/src/main/java/com/bdtopcoder/homework_247_2/SongList.java
@@ -2,6 +2,7 @@ package com.bdtopcoder.homework_247_2;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -23,119 +24,128 @@ import java.util.HashMap;
 
 public class SongList extends AppCompatActivity {
 
-    public static ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+   public static ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
 
-    boolean isPlayIcon = false;
+   boolean isPlayIcon = false;
 
-    ListView listView;
-    MediaPlayer mediaPlayer;
+   ListView listView;
+   MediaPlayer mediaPlayer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song_list);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      EdgeToEdge.enable(this);
+      setContentView(R.layout.activity_song_list);
+      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+         return insets;
+      });
 
-        // initialize
-        listView = findViewById(R.id.listView);
+      // initialize
+      listView = findViewById(R.id.listView);
 
-        // Adapter কে পরিচয় করিয়ে দেওয়া হয়েছে এবং list এর মধ্যে set করে দেওয়া হয়েছে ।
-        MyAdapter myAdapter = new MyAdapter();
-        listView.setAdapter(myAdapter);
+      // initialize audio
+      mediaPlayer = new MediaPlayer();
+      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
-    } // onCreate method end here =========
+      // Adapter কে পরিচয় করিয়ে দেওয়া হয়েছে এবং list এর মধ্যে set করে দেওয়া হয়েছে ।
+      MyAdapter myAdapter = new MyAdapter();
+      listView.setAdapter(myAdapter);
 
-    public class MyAdapter extends BaseAdapter {
+   } // onCreate method end here =========
 
-        @Override
-        public int getCount() {
-            return arrayList.size();
-        }
+   public class MyAdapter extends BaseAdapter {
 
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
+      @Override
+      public int getCount() {
+         return arrayList.size();
+      }
 
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
+      @Override
+      public Object getItem(int position) {
+         return null;
+      }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            @SuppressLint("ViewHolder") View myView = layoutInflater.inflate(R.layout.list_item, parent, false);
+      @Override
+      public long getItemId(int position) {
+         return 0;
+      }
 
-            // পরিচয় করিয়ে দেওয়া হয়েছে ।
-            TextView tvTitle = myView.findViewById(R.id.tvTitle);
-            TextView tvDescription = myView.findViewById(R.id.tvDescription);
-            ImageView tvSong = myView.findViewById(R.id.tvSong);
-            ImageView imgPlay = myView.findViewById(R.id.imgPlay);
+      @Override
+      public View getView(int position, View convertView, ViewGroup parent) {
+         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         @SuppressLint("ViewHolder") View myView = layoutInflater.inflate(R.layout.list_item, parent, false);
 
-            // list থেকে data get করা হয়েছে ।
-            HashMap<String, String> hashMap1 = arrayList.get(position);
-            String title = hashMap1.get("title");
-            String description = hashMap1.get("description");
-            String songImg = hashMap1.get("songImg");
-            String song = hashMap1.get("song");
+         // পরিচয় করিয়ে দেওয়া হয়েছে ।
+         TextView tvTitle = myView.findViewById(R.id.tvTitle);
+         TextView tvDescription = myView.findViewById(R.id.tvDescription);
+         ImageView tvSong = myView.findViewById(R.id.tvSong);
+         ImageView imgPlay = myView.findViewById(R.id.imgPlay);
 
-            // data list item এ set করে দেওয়া হয়েছে ।
-            tvTitle.setText(title);
-            tvDescription.setText(description);
-            tvSong.setImageResource(Integer.parseInt(songImg));
+         // list থেকে data get করা হয়েছে ।
+         HashMap<String, String> hashMap1 = arrayList.get(position);
+         String title = hashMap1.get("title");
+         String description = hashMap1.get("description");
+         String songImg = hashMap1.get("songImg");
+         String song = hashMap1.get("song");
 
-            int mySong = Integer.parseInt(song);
+         // data list item এ set করে দেওয়া হয়েছে ।
+         tvTitle.setText(title);
+         tvDescription.setText(description);
+         tvSong.setImageResource(Integer.parseInt(songImg));
 
-            // onClick লেখা হয়েছে ।
-            imgPlay.setOnClickListener(v -> {
+//         int mySong = Integer.parseInt(song);
 
-                if (imgPlay.getTag() != null && imgPlay.getTag().toString().contains("NOT_PLAYING")) {
-                    if (mediaPlayer != null) mediaPlayer.release();
-                    mediaPlayer = MediaPlayer.create(SongList.this, mySong);
-                    mediaPlayer.start();
-                    imgPlay.setImageResource(R.drawable.stop_icon);
-                    imgPlay.setTag("PLAYING_NOW");
+         // onClick লেখা হয়েছে ।
+         imgPlay.setOnClickListener(v -> {
+            try {
+               if (imgPlay.getTag() == null || imgPlay.getTag().toString().equals("NOT_PLAYING")) {
+                  if (mediaPlayer == null) {
+                     mediaPlayer = new MediaPlayer();
+                  } else {
+                     mediaPlayer.reset();
+                  }
 
-                    // when complete the song
-                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            imgPlay.setImageResource(R.drawable.play_icon);
-                            imgPlay.setTag("NOT_PLAYING");
-                        }
-                    });
+                  mediaPlayer.setDataSource(song);
+                  mediaPlayer.prepare();
+                  mediaPlayer.start();
 
-                    isPlayIcon = false;
+                  imgPlay.setImageResource(R.drawable.stop_icon);
+                  imgPlay.setTag("PLAYING_NOW");
 
-                } else {
-                    if (mediaPlayer != null) mediaPlayer.release();
-                    imgPlay.setImageResource(R.drawable.play_icon);
-                    imgPlay.setTag("NOT_PLAYING");
-                    isPlayIcon = true;
+                  // When playback is completed
+                  mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                     @Override
+                     public void onCompletion(MediaPlayer mp) {
+                        imgPlay.setImageResource(R.drawable.play_icon);
+                        imgPlay.setTag("NOT_PLAYING");
+                        mediaPlayer.release();
+                        mediaPlayer = null;
+                     }
+                  });
 
-                }
+               } else if (imgPlay.getTag().toString().equals("PLAYING_NOW")) {
+                  if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                     mediaPlayer.stop();
+                     mediaPlayer.release();
+                     mediaPlayer = null;
+                  }
 
-                // icon play reset
-                for (int xx = 0; xx < listView.getChildCount(); xx++) {
-                    View xxView = listView.getChildAt(xx);
-                    ImageView xxImageView = xxView.findViewById(R.id.imgPlay);
-                    xxImageView.setImageResource(R.drawable.play_icon);
-                }
-
-                if (!isPlayIcon){
-                    imgPlay.setImageResource(R.drawable.stop_icon);
-                }
-
-            });
-
-
-            return myView;
-        }
-
-
-    } // MyAdapter end here =================
+                  imgPlay.setImageResource(R.drawable.play_icon);
+                  imgPlay.setTag("NOT_PLAYING");
+               }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+         });
 
 
+         return myView;
+      }
+
+
+   } // MyAdapter end here =================
 
 
 } // public class end here ================
